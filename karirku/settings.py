b928ac060225a8b3bac2812426b612ccecaeb9b2
Emyr298 <emi.ru298@gmail.com>
2023-12-04 20:24:41 +0700

    feat: lowongan kerja migrations and installation

--- karirku/settings.py
+++ karirku/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'user',
     'ceritaalumni',
-    'informasikarier'
+    'informasikarier',
+    'lowongankerja',
 ]
 
 MIDDLEWARE = [
