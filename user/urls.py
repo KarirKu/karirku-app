81e757b21b12dfc355b6c4512d045eb43c9c50ad
michaelsinanta <michaelchristlambert@gmail.com>
2023-12-04 20:31:45 +0700

    add

--- user/urls.py
+++ user/urls.py
@@ -7,9 +7,9 @@ from rest_framework_simplejwt.views import (
 from . import views
 
 urlpatterns = [
-    path('register', views.Register.as_view(), name='user-register'),
-    path('login', TokenObtainPairView.as_view(), name='user-login'),
-    path('refresh', TokenRefreshView.as_view(), name='token-refresh'),
-    path('<uuid:pk>', views.UserView.as_view(), name='user-detail'),
+    path('register/', views.Register.as_view(), name='user-register'),
+    path('login/', TokenObtainPairView.as_view(), name='user-login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('<uuid:pk>/', views.UserView.as_view(), name='user-detail'),
     path('', views.CurrentUser.as_view(), name='current-user-detail'),
 ]
