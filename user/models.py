6424d58bace1f5824492e58afc6490a6b415740a
vinsen_wijaya <36811691+vwjaya32@users.noreply.github.com>
2023-12-11 19:31:13 +0700

    add user rekom

--- user/models.py
+++ user/models.py
@@ -40,6 +40,7 @@ class User(AbstractUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    rekomendasi_karier = models.TextField(null=True, blank=True)
     USER_CHOICES = [
         ('admin', 'Admin'),
         ('alumni', 'Alumni'),
